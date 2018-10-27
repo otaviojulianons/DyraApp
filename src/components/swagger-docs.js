fec591ad014a209b183f6c78e5c1a28d8b36e764
Otávio Nielsen <otavio.juliano.ns@gmail.com>
2018-10-27 02:15:34 -0300

    Added swagger cards description

--- src/components/swagger-docs.js
+++ src/components/swagger-docs.js
@@ -10,11 +10,11 @@ class SwaggerDoc extends Component {
         return ( <div className="col-md-offset-2 col-md-8 col-sm-12" style={styleContainer}>
               <SwaggerCard 
                 title="Swagger API"
-                description="description"
+                description="Entity registration documentation"
                 url={ `${process.env.REACT_APP_DYNAMIC_REST_API_URL}/swagger` }/>
               <SwaggerCard 
                 title="Dynamic Swagger"
-                description="description"
+                description="Dynamic Entity documentation"
                 url={ `${process.env.REACT_APP_DYNAMIC_REST_API_URL}/Dynamic/index.html` }/>
         </div> );
     }
